dfdc9d13a3c4d748cf15f49b8fca66fb1bd06184
John Karrell Speck <speckworks@gmail.com>
2019-08-22 00:33:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -213,5 +213,14 @@ end
 #   end
 #   teams 
 # end
+
+def player_numbers(team_name)
+  stats = {}
+  game_hash.each |team_name, number|
+    stats<<{:team_name =>"", :number}
+  end
+  stats
+end
+
   
   
